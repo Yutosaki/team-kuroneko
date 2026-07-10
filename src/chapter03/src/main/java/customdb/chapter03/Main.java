2aa28e36506450aa6c8c8b98c6d6ae36c0a3116c
Yutosaki <sasayu8190@gmail.com>
2026-07-10 20:31:44 +0900

    feat: add chapter03

---
+++ src/chapter03/src/main/java/customdb/chapter03/Main.java
@@ -0,0 +1,10 @@
+package customdb.chapter03;
+
+import customdb.chapter03.DB.nekoDB;
+
+public class Main {
+  public static void main(String[] args) {
+    nekoDB db = new nekoDB();
+    db.start();
+  }
+}
